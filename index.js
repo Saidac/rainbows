d7d9ffeef2f148d6a6434df96384be1e4f823808
William Saidac <sdfsfsds>
2016-10-07 14:41:16 +0200

    colors, gangs, the message, grand master flash

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
 var colors = require('colors/safe');
 console.log(colors.yellow('Who run the world?'));
 console.log(colors.rainbow('N3RD5!!!!!!!!'));
+
+var holla = 'Come to Daddy!';
+
+
+
+
+function rawPower() {
+console.log(colors.white(holla));
+console.log(colors.red(holla));
+console.log(colors.green(holla));
+console.log(colors.yellow(holla));
+}
+
+setInterval(rawPower, 500);
+
+
+
+
+
+
+
+
+
